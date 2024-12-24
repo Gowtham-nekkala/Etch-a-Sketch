c0779b43a06e36ae63042fe07f1f0765d59d7f42
Gowtham-nekkala <nekkalagowtham1801@gmail.com>
2024-12-24 16:25:49 +0530

    Started creating

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+const button = document.getElementById("resize");
+
+function Creategrid(size){
+    container.innerHTML = "";
+    const squaresize = 960/size;
+    for()
+}

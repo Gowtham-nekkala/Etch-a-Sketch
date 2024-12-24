6d0fb51932741b789607eadc39a7c81f3241d430
Gowtham-nekkala <nekkalagowtham1801@gmail.com>
2024-12-24 19:25:06 +0530

    Updated README and project is done!

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Etch-a-Sketch
+
+Hello guys.
+This is one of my practice exercise done from the odin project.
+
+implimented by DOM techniques.
--- script.js
+++ script.js
@@ -4,5 +4,29 @@ const button = document.getElementById("resize");
 function Creategrid(size){
     container.innerHTML = "";
     const squaresize = 960/size;
-    for()
+    for(let i = 0;i<size*size;i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${squaresize}px`;
+        square.style.height = `${squaresize}px`;
+
+        square.addEventListener('mousemove',()=>{
+            square.style.backgroundColor = '#000000';
+        });
+
+        container.appendChild(square);
+    }
 }
+
+button.addEventListener('click',() => {
+    const size = parseInt(prompt('Enter no.of boxes(max = 100)'));
+
+    if(size>0 && size<=100){
+        Creategrid(size);
+    }
+    else{
+        alert('Invalid!!!')
+    }
+});
+
+Creategrid(18);
